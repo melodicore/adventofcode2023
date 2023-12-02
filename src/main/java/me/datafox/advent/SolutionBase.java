0cfba9ef6b1057fa4734e989fcd2b58b30c47314
melodicore <datafox@datafox.me>
2023-12-02 18:58:07 +0200

    Initial setup

---
+++ src/main/java/me/datafox/advent/SolutionBase.java
@@ -0,0 +1,24 @@
+package me.datafox.advent;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author datafox
+ */
+public abstract class SolutionBase {
+    public void run() {
+        try(InputStream input = getClass().getResourceAsStream("/input.txt")) {
+            if(input == null) {
+                System.out.println("No input.txt in resources");
+                return;
+            }
+            System.out.println(solution(new String(input.readAllBytes())));
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected abstract String solution(String input);
+}
