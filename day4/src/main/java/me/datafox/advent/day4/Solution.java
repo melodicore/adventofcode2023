ae8e6e55ced82f66be5c4404cc7f3eb7e5279949
melodicore <datafox@datafox.me>
2023-12-04 10:36:19 +0200

    day 4 part 2

--- day4/src/main/java/me/datafox/advent/day4/Solution.java
+++ day4/src/main/java/me/datafox/advent/day4/Solution.java
@@ -2,7 +2,9 @@ package me.datafox.advent.day4;
 
 import me.datafox.advent.SolutionBase;
 
+import java.util.AbstractMap;
 import java.util.Arrays;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -27,7 +29,20 @@ public class Solution extends SolutionBase {
 
     @Override
     protected String solution2(String input) {
-        return "";
+        Map<Integer,Integer> map = input.lines()
+                .map(this::toScoreEntry)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        int[] cards = new int[map.size()];
+        Arrays.fill(cards, 1);
+        int[] sum = new int[1];
+        map.forEach((id, score) -> {
+            sum[0] += cards[id - 1];
+            for(int i = id; i < id + score; i++) {
+                if(i >= map.size()) break;
+                cards[i] += cards[id - 1];
+            }
+        });
+        return String.valueOf(sum[0]);
     }
 
     private int calculateScore(String game) {
@@ -35,16 +50,29 @@ public class Solution extends SolutionBase {
                 .replaceAll(" {2,}", " ")
                 .split(":")[1]
                 .split("\\|");
-        Set<Integer> numbers = Arrays
-                .stream(scores[0].strip().split(" "))
-                .map(Integer::parseInt)
-                .collect(Collectors.toSet());
-        Set<Integer> winning = Arrays
-                .stream(scores[1].strip().split(" "))
-                .map(Integer::parseInt)
-                .collect(Collectors.toSet());
+        Set<Integer> numbers = toNumberSet(scores[0]);
+        Set<Integer> winning = toNumberSet(scores[1]);
         long matches = numbers.stream().filter(winning::contains).count();
         if(matches == 0) return 0;
         return 1 << matches - 1;
+    }
+
+    private Set<Integer> toNumberSet(String numbers) {
+        return Arrays
+                .stream(numbers.strip().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toSet());
+    }
+
+    private Map.Entry<Integer,Integer> toScoreEntry(String game) {
+        String[] split = game
+                .replaceAll(" {2,}", " ")
+                .split(":");
+        int id = Integer.parseInt(split[0].split(" ")[1]);
+        String[] scores = split[1].split("\\|");
+        Set<Integer> numbers = toNumberSet(scores[0]);
+        Set<Integer> winning = toNumberSet(scores[1]);
+        long score = numbers.stream().filter(winning::contains).count();
+        return new AbstractMap.SimpleEntry<>(id, (int) score);
     }
 }
