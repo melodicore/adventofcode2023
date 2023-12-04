3a93cc8f55a0f14e5ace2d537806f24120b5f8a7
melodicore <datafox@datafox.me>
2023-12-04 10:18:50 +0200

    day 4 part 1

--- day4/src/main/java/me/datafox/advent/day4/Solution.java
+++ day4/src/main/java/me/datafox/advent/day4/Solution.java
@@ -2,6 +2,10 @@ package me.datafox.advent.day4;
 
 import me.datafox.advent.SolutionBase;
 
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 /**
  * Solution for advent of code day 4.
  *
@@ -16,11 +20,31 @@ public class Solution extends SolutionBase {
 
     @Override
     protected String solution1(String input) {
-        return "";
+        return String.valueOf(input.lines()
+                .mapToInt(this::calculateScore)
+                .sum());
     }
 
     @Override
     protected String solution2(String input) {
         return "";
+    }
+
+    private int calculateScore(String game) {
+        String[] scores = game
+                .replaceAll(" {2,}", " ")
+                .split(":")[1]
+                .split("\\|");
+        Set<Integer> numbers = Arrays
+                .stream(scores[0].strip().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toSet());
+        Set<Integer> winning = Arrays
+                .stream(scores[1].strip().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toSet());
+        long matches = numbers.stream().filter(winning::contains).count();
+        if(matches == 0) return 0;
+        return 1 << matches - 1;
     }
 }
