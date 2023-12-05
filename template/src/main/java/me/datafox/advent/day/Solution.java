a7068513afd5d4484ac37b3fe4236eec868c55c9
melodicore <datafox@datafox.me>
2023-12-05 12:19:44 +0200

    add template to copy

---
+++ template/src/main/java/me/datafox/advent/day/Solution.java
@@ -0,0 +1,26 @@
+package me.datafox.advent.day;
+
+import me.datafox.advent.SolutionBase;
+
+/**
+ * Solution for advent of code day ?.
+ *
+ * @see <a href=https://adventofcode.com/2023/day/?>https://adventofcode.com/2023/day/?</a>
+ *
+ * @author datafox
+ */
+public class Solution extends SolutionBase {
+    public static void main(String[] args) {
+        new Solution().run();
+    }
+
+    @Override
+    protected String solution1(String input) {
+        return "";
+    }
+
+    @Override
+    protected String solution2(String input) {
+        return "";
+    }
+}
