e180da1561f219d7388883c15d9a719d44d2ec44
melodicore <datafox@datafox.me>
2023-12-06 11:30:19 +0200

    day 6 part 1

--- day6/src/main/java/me/datafox/advent/day6/Solution.java
+++ day6/src/main/java/me/datafox/advent/day6/Solution.java
@@ -2,6 +2,8 @@ package me.datafox.advent.day6;
 
 import me.datafox.advent.SolutionBase;
 
+import java.util.Arrays;
+
 /**
  * Solution for advent of code day 6.
  *
@@ -16,7 +18,24 @@ public class Solution extends SolutionBase {
 
     @Override
     protected String solution1(String input) {
-        return "";
+        String[] split = input.split("\n");
+        int[] time = Arrays.stream(split[0].split(" +", 2)[1].split(" +")).mapToInt(Integer::parseInt).toArray();
+        int[] distance = Arrays.stream(split[1].split(" +", 2)[1].split(" +")).mapToInt(Integer::parseInt).toArray();
+        System.out.println(Arrays.toString(time));
+        System.out.println(Arrays.toString(distance));
+        int result = 1;
+        for(int i = 0; i < time.length; i++) {
+            int ways = 0;
+            for(int j = 1; j < time[i]; j++) {
+                if(j * (time[i] - j) > distance[i]) {
+                    System.out.println(time[i] + ", " + distance[i] + ", " + j);
+                    ways++;
+                }
+            }
+            System.out.println(time[i] + ", " + distance[i] + ", " + ways);
+            result *= ways;
+        }
+        return String.valueOf(result);
     }
 
     @Override
