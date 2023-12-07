678abc0158ae35e87ec5ec9b09416cdf83295061
melodicore <datafox@datafox.me>
2023-12-07 11:28:13 +0200

    start day 7

---
+++ day7/src/main/java/me/datafox/advent/day7/Solution.java
@@ -0,0 +1,26 @@
+package me.datafox.advent.day7;
+
+import me.datafox.advent.SolutionBase;
+
+/**
+ * Solution for advent of code day 7.
+ *
+ * @see <a href=https://adventofcode.com/2023/day/7>https://adventofcode.com/2023/day/7</a>
+ *
+ * @author datafox
+ */
+public class Solution extends SolutionBase {
+    public static void main(String[] args) {
+        new Solution().run();
+    }
+
+    @Override
+    protected String solution1(String input) {
+        return "";
+    }
+
+    @Override
+    protected String solution2(String input) {
+        return "";
+    }
+}
