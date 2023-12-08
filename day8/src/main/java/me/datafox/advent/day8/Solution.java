81d7834753a7298ce9c1c8aed912253131a0c6e4
melodicore <datafox@datafox.me>
2023-12-08 13:56:49 +0200

    day 8 part 2 done

--- day8/src/main/java/me/datafox/advent/day8/Solution.java
+++ day8/src/main/java/me/datafox/advent/day8/Solution.java
@@ -2,14 +2,15 @@ package me.datafox.advent.day8;
 
 import me.datafox.advent.SolutionBase;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
- * Solution for advent of code day ?.
+ * Solution for advent of code day 8.
  *
- * @see <a href=https://adventofcode.com/2023/day/?>https://adventofcode.com/2023/day/?</a>
+ * @see <a href=https://adventofcode.com/2023/day/8>https://adventofcode.com/2023/day/8</a>
  *
  * @author datafox
  */
@@ -40,6 +41,46 @@ public class Solution extends SolutionBase {
         return String.valueOf(i);
     }
 
+    @Override
+    protected String solution2(String input) {
+        String[] split = input.split("\n\n");
+        char[] guide = split[0].toCharArray();
+        Map<String,Node> nodes = new HashMap<>(split[1]
+                .lines()
+                .collect(Collectors.toMap(
+                        this::getKey,
+                        this::getNode)));
+        Collection<MappedNode> mappedNodes = mapNodes(nodes);
+        MappedNode[] currents = mappedNodes
+                .parallelStream()
+                .filter(node -> node.id.endsWith("A"))
+                .toArray(MappedNode[]::new);
+        BigInteger[] repeats = new BigInteger[currents.length];
+        for(int i = 0; i < currents.length; i++) {
+            long counter = 0;
+            while(true) {
+                currents[i] = guide[(int) (counter % guide.length)] == 'L' ? currents[i].left : currents[i].right;
+                counter++;
+                if(currents[i].id.endsWith("Z")) {
+                    repeats[i] = BigInteger.valueOf(counter);
+                    break;
+                }
+            }
+        }
+        BigInteger result = BigInteger.ZERO;
+        for(BigInteger bi : repeats) {
+            if(result.equals(BigInteger.ZERO)) {
+                result = bi;
+            } else {
+                BigInteger gcd = result.gcd(bi);
+                BigInteger abs = result.multiply(bi).abs();
+                result = abs.divide(gcd);
+            }
+        }
+        
+        return result.toString();
+    }
+
     private String getKey(String line) {
         return line.substring(0, 3);
     }
@@ -48,10 +89,29 @@ public class Solution extends SolutionBase {
         return new Node(line.substring(7, 10), line.substring(12, 15));
     }
 
-    @Override
-    protected String solution2(String input) {
-        return "";
+    private Collection<MappedNode> mapNodes(Map<String,Node> map) {
+        Map<String,MappedNode> temp = map
+                .keySet()
+                .stream()
+                .collect(Collectors.toMap(
+                        Function.identity(),
+                        MappedNode::new));
+        map.forEach((id, node) -> {
+            MappedNode mapped = temp.get(id);
+            mapped.left = temp.get(node.left);
+            mapped.right = temp.get(node.right);
+        });
+        return temp.values();
     }
 
     private record Node(String left, String right) {}
+
+    private static class MappedNode {
+        private final String id;
+        private MappedNode left, right;
+
+        private MappedNode(String id) {
+            this.id = id;
+        }
+    }
 }
