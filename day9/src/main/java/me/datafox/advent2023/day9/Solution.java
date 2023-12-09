6f82665f74b253230d142fb1dc4a6c3621cb7709
melodicore <datafox@datafox.me>
2023-12-09 10:08:15 +0200

    day 9 part 1

--- day9/src/main/java/me/datafox/advent2023/day9/Solution.java
+++ day9/src/main/java/me/datafox/advent2023/day9/Solution.java
@@ -2,6 +2,11 @@ package me.datafox.advent2023.day9;
 
 import me.datafox.advent2023.SolutionBase;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Solution for advent of code 2023 day 9.
  *
@@ -16,11 +21,55 @@ public class Solution extends SolutionBase {
 
     @Override
     protected String solution1(String input) {
-        return "";
+        return String.valueOf(input
+                .lines()
+                .map(this::getSequence)
+                .map(this::fillSequence)
+                .mapToInt(this::getNext)
+                .sum());
     }
 
     @Override
     protected String solution2(String input) {
         return "";
+    }
+
+    private int[] getSequence(String s) {
+        return Arrays
+                .stream(s.split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+    }
+
+    private List<int[]> fillSequence(int[] ints) {
+        List<int[]> list = new ArrayList<>();
+        list.add(ints);
+        boolean running = true;
+        while(running) {
+            int[] arr = new int[ints.length - 1];
+            boolean finished = true;
+            for(int i = 0; i < arr.length; i++) {
+                arr[i] = ints[i + 1] - ints[i];
+                if(arr[i] != 0) {
+                    finished = false;
+                }
+            }
+            list.add(arr);
+            if(finished) {
+                running = false;
+            } else {
+                ints = arr;
+            }
+        }
+        return list;
+    }
+
+    private int getNext(List<int[]> list) {
+        int result = 0;
+        Collections.reverse(list);
+        for(int[] ints : list) {
+            result += ints[ints.length - 1];
+        }
+        return result;
     }
 }
