b1f582f9ff4b142bafdcf74438db09336033db44
melodicore <datafox@datafox.me>
2023-12-09 09:51:30 +0200

    start day 9

---
+++ day9/src/main/java/me/datafox/advent2023/day9/Solution.java
@@ -0,0 +1,26 @@
+package me.datafox.advent2023.day9;
+
+import me.datafox.advent2023.SolutionBase;
+
+/**
+ * Solution for advent of code 2023 day 9.
+ *
+ * @see <a href=https://adventofcode.com/2023/day/9>https://adventofcode.com/2023/day/9</a>
+ *
+ * @author datafox
+ */
+public class Solution extends SolutionBase {
+    public static void main(String[] args) {
+        new Solution().run();
+    }
+
+    @Override
+    protected String solution1(String input) {
+        return "";
+    }
+
+    @Override
+    protected String solution2(String input) {
+        return "";
+    }
+}
