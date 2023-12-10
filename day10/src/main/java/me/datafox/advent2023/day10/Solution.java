f75f89e2bbb1a6038e058bc73be351d605b92888
melodicore <datafox@datafox.me>
2023-12-10 12:38:56 +0200

    start day 10

---
+++ day10/src/main/java/me/datafox/advent2023/day10/Solution.java
@@ -0,0 +1,26 @@
+package me.datafox.advent2023.day10;
+
+import me.datafox.advent2023.SolutionBase;
+
+/**
+ * Solution for advent of code 2023 day 10.
+ *
+ * @see <a href=https://adventofcode.com/2023/day/10>https://adventofcode.com/2023/day/10</a>
+ *
+ * @author datafox
+ */
+public class Solution extends SolutionBase {
+    public static void main(String[] args) {
+        new Solution().run();
+    }
+
+    @Override
+    protected String solution1(String input) {
+        return "";
+    }
+
+    @Override
+    protected String solution2(String input) {
+        return "";
+    }
+}
