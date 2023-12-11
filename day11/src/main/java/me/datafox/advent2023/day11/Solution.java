757db955f959910eed9f23f397d08c420e1368f1
melodicore <datafox@datafox.me>
2023-12-11 14:20:05 +0200

    day 11 part 2

--- day11/src/main/java/me/datafox/advent2023/day11/Solution.java
+++ day11/src/main/java/me/datafox/advent2023/day11/Solution.java
@@ -2,7 +2,10 @@ package me.datafox.advent2023.day11;
 
 import me.datafox.advent2023.SolutionBase;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.BiFunction;
+import java.util.function.ToLongFunction;
 import java.util.stream.Collectors;
 
 /**
@@ -19,6 +22,21 @@ public class Solution extends SolutionBase {
 
     @Override
     protected String solution1(String input) {
+        Set<Universe> universes = getUniverses(input);
+        universes = expand(universes, 1, Universe::x, (u, l) -> u.add(l, 0));
+        universes = expand(universes, 1, Universe::y, (u, l) -> u.add(0, l));
+        return String.valueOf(distanceSum(universes));
+    }
+
+    @Override
+    protected String solution2(String input) {
+        Set<Universe> universes = getUniverses(input);
+        universes = expand(universes, 999999, Universe::x, (u, l) -> u.add(l, 0));
+        universes = expand(universes, 999999, Universe::y, (u, l) -> u.add(0, l));
+        return String.valueOf(distanceSum(universes));
+    }
+
+    private Set<Universe> getUniverses(String input) {
         String[] lines = input.split("\n");
         Set<Universe> universes = new HashSet<>();
         for(int y = 0; y < lines.length; y++) {
@@ -28,52 +46,43 @@ public class Solution extends SolutionBase {
                 }
             }
         }
+        return universes;
+    }
+
+    private Set<Universe> expand(Set<Universe> universes, long expand, ToLongFunction<Universe> get, BiFunction<Universe,Long,Universe> add) {
         Set<Universe> expanded = new HashSet<>();
-        int[] expansion = { 0 };
-        for(int[] x = { 0 }; x[0] < lines[0].length(); x[0]++) {
-            Set<Universe> current = universes.stream().filter(u -> u.x == x[0]).collect(Collectors.toSet());
+        long[] expansion = { 0 };
+        for(int[] i = { 0 }; i[0] <= universes.stream().mapToLong(get).max().orElseThrow(); i[0]++) {
+            Set<Universe> current = universes.stream().filter(u -> get.applyAsLong(u) == i[0]).collect(Collectors.toSet());
             if(current.isEmpty()) {
-                expansion[0]++;
+                expansion[0] += expand;
             } else {
-                expanded.addAll(current.stream().map(u -> u.add(expansion[0], 0)).collect(Collectors.toSet()));
+                expanded.addAll(current.stream().map(u -> add.apply(u, expansion[0])).collect(Collectors.toSet()));
             }
         }
-        universes = expanded;
-        expanded = new HashSet<>();
-        expansion[0] = 0;
-        for(int[] y = { 0 }; y[0] < lines.length; y[0]++) {
-            Set<Universe> current = universes.stream().filter(u -> u.y == y[0]).collect(Collectors.toSet());
-            if(current.isEmpty()) {
-                expansion[0]++;
-            } else {
-                expanded.addAll(current.stream().map(u -> u.add(0, expansion[0])).collect(Collectors.toSet()));
-            }
-        }
-        List<Universe> universeList = new ArrayList<>(expanded);
+        return expanded;
+    }
+
+    private long distanceSum(Set<Universe> universes) {
         Set<Universe> visited = new HashSet<>();
-        int sum = 0;
-        for(Universe u : universeList) {
+        long sum = 0;
+        for(Universe u : universes) {
             visited.add(u);
-            for(Universe u1 : universeList) {
+            for(Universe u1 : universes) {
                 if(!visited.contains(u1)) {
                     sum += u.distance(u1);
                 }
             }
         }
-        return String.valueOf(sum);
+        return sum;
     }
 
-    @Override
-    protected String solution2(String input) {
-        return "";
-    }
-
-    private record Universe(int x, int y) {
-        public Universe add(int x, int y) {
+    private record Universe(long x, long y) {
+        public Universe add(long x, long y) {
             return new Universe(this.x + x, this.y + y);
         }
 
-        public int distance(Universe o) {
+        public long distance(Universe o) {
             return Math.abs(x - o.x) + Math.abs(y - o.y);
         }
     }
